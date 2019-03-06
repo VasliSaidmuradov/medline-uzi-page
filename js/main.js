d7cc4bda93cff33e1d2dd857b878686194b59dab
Vasli Saidmuradov <vasli.said@gmail.com>
2019-03-06 13:04:39 +0600

    with animation in mobile version

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-$(window).resize(function () {
-  var viewportWidth = $(window).width();
-  if (viewportWidth < 600) {
-    $(".animatedParent").removeClass("animatedParent");
-  }
-});
+// var target = $(".order-mobile");
+// var targetPos = target.offset().top;
+// var winHeight = $(window).height();
+// var scrollToElem = targetPos - winHeight;
+// $(window).scroll(function () {
+//   var winScrollTop = $(this).scrollTop();
+//   if (winScrollTop > scrollToElem) {
+//     //сработает когда пользователь доскроллит к элементу с классом .order-mobile
+//     var div = $(".row_item");
+
+//     // div.animate({
+//     //   left: "100vw"
+//     // }, "3s");
+//     div.animate({
+//       left: "0"
+//     }, "slow");
+//   }
+// });
